809905d31f0c52877efdee734e8f4009f6e19f9e
Jeet Patel <jpatel99967@gmail.com>
2020-10-24 00:09:25 +0530

    mobile number issue fixed

--- system_users/models.py
+++ system_users/models.py
@@ -39,7 +39,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=255, blank=False)
 
     email = models.EmailField(max_length=255, blank=False, unique= True)
-    mobile_number = models.BigIntegerField(blank=True, unique=True)
+    mobile_number = models.BigIntegerField(blank=True)
     
     company = models.ForeignKey(CompanyDetails, on_delete=models.CASCADE, blank=True, null=True)
         
