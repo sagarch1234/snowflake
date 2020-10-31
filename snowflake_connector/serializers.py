9eff206b1f8df889a05a5b4a540e201b4adb8ad0
Jeet Patel <jpatel99967@gmail.com>
2020-10-31 12:19:21 +0530

    Add instance to the system.

--- snowflake_connector/serializers.py
+++ snowflake_connector/serializers.py
@@ -9,3 +9,8 @@ class InstancesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Instances
         fields = ['id', 'instance_name', 'instance_user', 'instance_password', 'instance_role', 'company', 'instance_account', 'created_by']
+        extra_kwargs = {
+            'created_by' : {
+                'required' : False
+            }
+        }
